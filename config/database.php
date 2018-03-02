51bed73804c8f162754c87ba9765818650ba2468
Aleksandr Ayvazov <mefist.vip@gmail.com>
2018-03-02 19:27:18 +0300

    Update database.php

--- config/database.php
+++ config/database.php
@@ -55,9 +55,9 @@ return [
         'mysql'  => [
             'driver'    => 'mysql',
             'host'      => env('DB_HOST', 'localhost'),
-            'database'  => env('DB_DATABASE', 'lavalite-shoping'),
-            'username'  => env('DB_USERNAME', 'root'),
-            'password'  => env('DB_PASSWORD', 'Vertex25'),
+            'database'  => env('DB_DATABASE', 'database'),
+            'username'  => env('DB_USERNAME', 'username'),
+            'password'  => env('DB_PASSWORD', 'password'),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
